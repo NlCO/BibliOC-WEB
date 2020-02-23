5f319147c7a335ae102139d9f20ee8acbb984ad9
Nicolas <nicolas.lejeune@axa.fr>
2020-02-23 23:30:19 +0100

    feat(): Add custom login page

--- src/main/java/fr/nlco/biblioc/bibliocweb/controller/HomeController.java
+++ src/main/java/fr/nlco/biblioc/bibliocweb/controller/HomeController.java
@@ -1,6 +1,7 @@
 package fr.nlco.biblioc.bibliocweb.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
@@ -17,5 +18,18 @@ public class HomeController {
     @GetMapping("/")
     public String getHomePage() {
         return "home";
+    }
+
+    /**
+     * Affichage de la page de login
+     *
+     * @param model contient les infos pour la vue
+     * @param error message d'erreur passé à la vue
+     * @return la page de login
+     */
+    @GetMapping("/login")
+    public String login(Model model, String error) {
+        if (error != null) model.addAttribute("error", "Saisie incorrecte de vos identifiants");
+        return "login";
     }
 }
